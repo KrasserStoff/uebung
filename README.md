43ee25213f21ecfb19542cb93627bf7d2da2bb38
Serafin Dinges <dingesserafin@gmail.com>
2022-07-25 09:43:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# uebung
+
+- run `npm install` to initialise
+- run `npx live-server` to start app
+
+## Aufgaben
+
+- [ ] Das Formular soll folgende Felder haben: Name, Adresse, E-Mail und Checkbox für AGB.
+- [ ] Der Text auf dem Button des Formulars soll "Absenden" lauten.
+- [ ] Wenn das Formular **richtig** ausgefüllt wurde und absenden geklickt wurde, sollen die Daten über die bereitgestellte Methode `sendData` abgeschickt werden.
+- [ ] Das Formular soll im Anschluss durch eine Erfolgsnachricht ersetzt werden.
+- [ ] Das Logo von krasserstoff.com soll oben über dem Formular angezeigt werden. (Alle Dateien bitte in den `/assets` ordner )
+- [ ] Kopiere das Hintergrundbild von krasserstoff.com in den Hintergrund der ganzen Website.
---
+++ assets/index.js
@@ -0,0 +1,41 @@
+function runApp() {
+	const form = document.getElementById("form");
+
+	form.addEventListener("submit", submitForm);
+
+	window.airbrake = new Airbrake.Notifier({
+		projectId: 187636,
+		projectKey: "77b1823da05706642d8b4aa1e62e7742",
+		environment: "uebung",
+	});
+}
+
+runApp();
+
+function submitForm(event) {
+	event.preventDefault();
+	console.log(getFormValue("name"));
+
+	var formData = {
+		name: getFormValue("name"),
+	};
+
+	sendData(formData);
+}
+
+// returns the value of an input field by ID
+function getFormValue(id) {
+	return document.getElementById(id).value;
+}
+
+// sends data log to airbrake for documentation
+function sendData(formData) {
+	airbrake.notify({
+		error: "Übung",
+		context: {
+			component: "uebung",
+			severity: "info",
+		},
+		params: formData,
+	});
+}
